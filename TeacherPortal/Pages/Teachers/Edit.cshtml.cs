86e00f42b340cfc936ff391b0ca069e705fbef24
Cooper Hiebendaal <ac99741@avcol.school.nz>
2020-09-02 14:34:21 +1200

    Edit Razor Page created, used for changing the details of the Teachers

---
+++ TeacherPortal/Pages/Teachers/Edit.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TeacherDirectory.Models;
+using TeacherDirectory.Services;
+
+namespace TeacherPortal.Pages.Teachers
+{
+    public class EditModel : PageModel
+    {
+        private readonly ITeacherRepository teacherRepository;
+
+        public EditModel(ITeacherRepository teacherRepository)
+        {
+            this.teacherRepository = teacherRepository;
+        }
+
+        public Teacher Teacher { get; set; } 
+
+        public IActionResult OnGet(int ID)
+        {
+          Teacher = teacherRepository.GetTeacher(ID);
+
+          if (Teacher == null)
+            {
+                return RedirectToPage("/NotFound");
+            }
+
+            return Page();
+        }
+    }
+}
