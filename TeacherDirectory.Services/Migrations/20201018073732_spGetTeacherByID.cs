c9091ebfc7c5055d29f8e3d19010baf746109c07
Cooper Hiebendaal <ac99741@avcol.school.nz>
2020-10-19 08:45:05 +1300

    Created Stored Procedures

---
+++ TeacherDirectory.Services/Migrations/20201018073732_spGetTeacherByID.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeacherDirectory.Services.Migrations
+{
+    public partial class spGetTeacherByID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string procedure = @"Create procedure spGetTeacherByID
+@ID int
+as
+Begin
+	Select * from Teachers
+	where ID = @ID
+End";
+            migrationBuilder.Sql(procedure);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string procedure = @"Drop procedure spGetTeacherByID";
+            migrationBuilder.Sql(procedure);
+        }
+    }
+}
--- TeacherDirectory.Services/SQLTeacherRepository.cs
+++ TeacherDirectory.Services/SQLTeacherRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,8 +19,11 @@ namespace TeacherDirectory.Services
 
         public Teacher Add(Teacher newTeacher)
         {
-            context.Teachers.Add(newTeacher);
-            context.SaveChanges();
+            context.Database.ExecuteSqlRaw("spInsertTeacher {0}, {1}, {2}, {3}",
+                newTeacher.FirstName,
+                newTeacher.Email,
+                newTeacher.PhotoPath,
+                newTeacher.Department);
             return newTeacher;
         }
 
@@ -35,12 +40,19 @@ namespace TeacherDirectory.Services
 
         public IEnumerable<Teacher> GetAllTeachers()
         {
-            return context.Teachers;
+            return context.Teachers
+                     .FromSqlRaw("SELECT * FROM Teachers")
+                     .ToList();
         }
 
         public Teacher GetTeacher(int ID)
         {
-            return context.Teachers.Find(ID);
+            SqlParameter parameter = new SqlParameter("@ID", ID);
+
+            return context.Teachers
+                .FromSqlRaw<Teacher>("spGetTeacherByID @ID", parameter)
+                .ToList()
+                .FirstOrDefault();
         }
 
         public IEnumerable<Teacher> Search(string searchTerm)
